f1e95e5f699a4aef4d48e0047e79981e116d9ffe
Marxes <pitzkemar@gmail.com>
2024-03-11 09:22:28 +0100

    Level1

--- src/Level/Level4.java
+++ src/Level/Level4.java
@@ -2,7 +2,9 @@ package src.Level;
 
 import src.Coords;
 import src.Objekte.Baubar.Basis.Basis;
+import src.Objekte.Monster.Bombenschiff;
 import src.Objekte.Monster.DefaultMonster;
+import src.Objekte.Monster.Lakai;
 
 public class Level4 extends Level{
     public Level4(Basis basis){
@@ -13,26 +15,17 @@ public class Level4 extends Level{
                 new Coords(39, 4),
                 basis,
                 new Coords(0, 18),
-                80,
+                140,
                 30
         );
-        monstersToSpawn.add(new DefaultMonster(new Coords(-1, -1)));
-        monstersToSpawn.add(new DefaultMonster(new Coords(-1, -1)));
-        monstersToSpawn.add(new DefaultMonster(new Coords(-1, -1)));
-        monstersToSpawn.add(new DefaultMonster(new Coords(-1, -1)));
-        monstersToSpawn.add(new DefaultMonster(new Coords(-1, -1)));
-        monstersToSpawn.add(new DefaultMonster(new Coords(-1, -1)));
-        monstersToSpawn.add(new DefaultMonster(new Coords(-1, -1)));
-        monstersToSpawn.add(new DefaultMonster(new Coords(-1, -1)));
-        monstersToSpawn.add(new DefaultMonster(new Coords(-1, -1)));
-        monstersToSpawn.add(new DefaultMonster(new Coords(-1, -1)));
-        monstersToSpawn.add(new DefaultMonster(new Coords(-1, -1)));
-        monstersToSpawn.add(new DefaultMonster(new Coords(-1, -1)));
-        monstersToSpawn.add(new DefaultMonster(new Coords(-1, -1)));
-        monstersToSpawn.add(new DefaultMonster(new Coords(-1, -1)));
-        monstersToSpawn.add(new DefaultMonster(new Coords(-1, -1)));
-        monstersToSpawn.add(new DefaultMonster(new Coords(-1, -1)));
-        basis.setHealth(basis.getMaxHealth());
+        monstersToSpawn.add(new Lakai(new Coords(-1, -1)));
+        monstersToSpawn.add(new Lakai(new Coords(-1, -1)));
+        monstersToSpawn.add(new Lakai(new Coords(-1, -1)));
+        monstersToSpawn.add(new Lakai(new Coords(-1, -1)));
+        monstersToSpawn.add(new Lakai(new Coords(-1, -1)));
+        monstersToSpawn.add(new Lakai(new Coords(-1, -1)));
+        monstersToSpawn.add(new Bombenschiff(new Coords(-1, -1)));
+        monstersToSpawn.add(new Bombenschiff(new Coords(-1, -1)));
         basis.setPosition(new Coords(9, 4));
     }
 }
