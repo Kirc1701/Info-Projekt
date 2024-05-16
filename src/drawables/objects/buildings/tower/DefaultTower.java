478db19a6b69c0eef0776860d92e4b77e930e33a
Samiel <samuel@schwenn.biz>
2024-05-16 20:04:56 +0200

    Renamed the files to be continuously in English; Refactored Drawables and made shots fancy
    Signed-off-by: Samiel <samuel@schwenn.biz>

---
+++ src/drawables/objects/buildings/tower/DefaultTower.java
@@ -0,0 +1,43 @@
+package src.drawables.objects.buildings.tower;
+
+import src.LogicRepresentation;
+import src.drawables.objects.monster.BombCarrier;
+import src.drawables.objects.monster.Monster;
+import src.util.CoordsInt;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+import java.util.Objects;
+
+import static src.visuals.GameScreen.spaceBetweenLinesPixels;
+
+public class DefaultTower extends Tower {
+    private static final Image image;
+
+    static {
+        try {
+            image = ImageIO.read(Objects.requireNonNull(BombCarrier.class.getClassLoader().getResourceAsStream("images/DefaultTurm.png"))).getScaledInstance(spaceBetweenLinesPixels, spaceBetweenLinesPixels, Image.SCALE_SMOOTH);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public DefaultTower(CoordsInt position) {
+        super(5, 25, position, 4, 6, 20, "DefaultTower");
+    }
+
+    @Override
+    public Monster[] shoot(LogicRepresentation logicRepresentation) {
+        return new Monster[]{shootNormal(logicRepresentation)};
+    }
+
+    @Override
+    public Image getImage() {
+        return image;
+    }
+
+    public static Image getStaticImage() {
+        return image;
+    }
+}
